400b7e62bb4b3a02f8d2eace41df86f3a2b9553c
mr1xyyy <oybeknarimonov@gmail.com>
2026-08-29 22:45:28 +0500

    fix images

--- assets/js/utils/images.js
+++ assets/js/utils/images.js
@@ -2,6 +2,9 @@ window.FischUtils = window.FischUtils || {};
 
 const IMAGE_OVERRIDES = {
   "Anchor & Chain": "Anchor_n'_Chain.png",
+  "Azure of Lagoon": "Azure_Of_Lagoon.png",
+  "Blade of Glorp": "Blade_Of_Glorp.png",
+  "Brine-infused Rod": "Brine-Infused_Rod.png",
   "Challengers Rod": "Challenger's_Rod.png",
   "Cryolash Rod": "Cryolash.png",
   "Crystallised Rod": "Crystalized_Rod.png",
@@ -15,6 +18,11 @@ const IMAGE_OVERRIDES = {
   "Pinions Aria": "Pinion's_Aria.png",
   "Poseidon Lance": "Poseidon's_Lance.png",
   "Requiem Rod": "Requiem.png",
+  "Rod of the Depths": "Rod_Of_The_Depths.png",
+  "Rod of the Eternal King": "Rod_Of_The_Eternal_King.png",
+  "Rod of the Exalted One": "Rod_Of_The_Exalted_One.png",
+  "Rod of the Forgotten Fang": "Rod_Of_The_Forgotten_Fang.png",
+  "Rod of the Zenith": "Rod_Of_The_Zenith.png",
   "Scarlet Spincaster": "Scarlet_Spincaster_Rod.png",
   "Thalassar Ruin": "Thalassar's_Ruin.png",
   "Boom Ball": "The_Boom_Ball.png",
