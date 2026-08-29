89e888aa01fc5bd37279b823681125d8ef0166f3
mr1xyyy <oybeknarimonov@gmail.com>
2026-08-29 22:57:02 +0500

    fix smth

--- assets/js/utils/wiki.js
+++ assets/js/utils/wiki.js
@@ -1,6 +1,21 @@
 window.FischUtils = window.FischUtils || {};
 
+const WIKI_PAGE_OVERRIDES = {
+  "Anchor & Chain": "Anchor n' Chain",
+  "Cryolash Rod": "Cryolash",
+  "Crystallised Rod": "Crystalized Rod",
+  "Duskwire Rod": "Duskwire",
+  "Elder Mossripper Rod": "Elder Mossripper",
+  "Nico's Yarncaster Rod": "Nico's Yarncaster",
+  "NoLife Rod": "No-Life Rod",
+  "Poseidon Lance": "Poseidon's Lance",
+  "Requiem Rod": "Requiem",
+  "Thalassar Ruin": "Thalassar's Ruin",
+  "Zeus Thundermaul": "Zeus's Thundermaul",
+};
+
 window.FischUtils.wikiUrl = function wikiUrl(name){
-  const page = encodeURIComponent(name.replace(/\s+/g, '_'));
+  const pageTitle = WIKI_PAGE_OVERRIDES[name] || name;
+  const page = encodeURIComponent(pageTitle.replace(/\s+/g, '_'));
   return `https://fischipedia.org/wiki/${page}`;
 };
